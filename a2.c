92706a3438c05ef385905aed17145f514e5d7fa3
TDS02 <TDS02@DSN119-001.edu119.local>
2023-04-14 11:06:20 -0300

    a2.c changed to print the lyrics of Viva la Vida

--- a2.c
+++ a2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("From fairest creatures we desire increase");
+    printf("I used to rule the world\nSeas would rise when I gave the word\nNow in the morning i sleep alone\nSweep the streets I used to rollz\nI hear Jerusalem bells ringing");
     return 0;
 }
